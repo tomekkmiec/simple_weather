a0cf202bd7896bba1ffbf5c4a82b4b5291202815
Tomek Kmiec <t.kmiec171@gmail.com>
2022-07-05 14:01:20 +0200

    Add dio hangling errors

--- lib/data/data/remote_data_sources/weather_remote_data_source.dart
+++ lib/data/data/remote_data_sources/weather_remote_data_source.dart
@@ -4,8 +4,13 @@ class WeatherRemoteDataSource {
   Future<Map<String, dynamic>?> getWeatherData({
     required String city,
   }) async {
-    final response = await Dio().get<Map<String, dynamic>>(
-        'http://api.weatherapi.com/v1/current.json?key=afe3c2c7afe049e8862203238221906&q=$city&aqi=no');
-    return response.data;
+    try {
+      final response = await Dio().get<Map<String, dynamic>>(
+          'http://api.weatherapi.com/v1/current.json?key=afe3c2c7afe049e8862203238221906&q=$city&aqi=no');
+      return response.data;
+    } on DioError catch (error) {
+      throw Exception(
+          error.response?.data['error']['message'] ?? 'Unknown error');
+    }
   }
 }
